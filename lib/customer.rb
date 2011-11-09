a5df997ffcb99cca0ff2e64b1b90762921c51429
Richard Patching <richard@justaddpixels.com>
2011-11-09 15:27:40 +0000

    Add customer

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,4 +1,14 @@
 module FastSpring
   class Customer
+    attr_accessor :first_name, :last_name, :company, :email, :phone_number
+
+    def initialize(details)
+      @first_name = details.fetch('firstName')
+      @last_name = details.fetch('lastName')
+      @company = details.fetch('company')
+      @email = details.fetch('email')
+      @phone_number = details.fetch('phoneNumber')
+    end
+
   end
 end
---
+++ spec/customer_spec.rb
@@ -0,0 +1,37 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '../lib/customer.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper.rb'))
+
+describe FastSpring::Customer do
+  context 'new' do
+    let(:customer_details) {
+      {
+        "firstName" => "John",
+        "lastName" => "Doe",
+        "company" => "Doe Inc",
+        "email" => "john@example.com",
+        "phoneNumber" => "0123456789"
+      }
+    }
+    subject { FastSpring::Customer.new(customer_details) }
+
+    it 'has a first name' do
+      subject.first_name.should == 'John'
+    end
+
+    it 'has a last name' do
+      subject.last_name.should == 'Doe'
+    end
+
+    it 'has a company' do
+      subject.company.should == 'Doe Inc'
+    end
+
+    it 'has a email address' do
+      subject.email.should == 'john@example.com'
+    end
+
+    it 'has a phone number' do
+      subject.phone_number.should == '0123456789'
+    end
+  end
+end
