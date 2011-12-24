3ff5b4b8317837f528ca22989856a0eba6329f09
Pascal Lindelauf <pascal@lindelauf.com>
2011-12-24 08:56:47 +0100

    Provisioned for partially filled customer data

--- lib/fastspring-saasy/customer.rb
+++ lib/fastspring-saasy/customer.rb
@@ -3,11 +3,11 @@ module FastSpring
     attr_accessor :first_name, :last_name, :company, :email, :phone_number
 
     def initialize(details)
-      @first_name = details.fetch('firstName')
-      @last_name = details.fetch('lastName')
-      @company = details.fetch('company')
+      @first_name = details.fetch('firstName', '')
+      @last_name = details.fetch('lastName', '') 
+      @company = details.fetch('company', '') 
       @email = details.fetch('email')
-      @phone_number = details.fetch('phoneNumber')
+      @phone_number = details.fetch('phoneNumber', '')
     end
 
   end
