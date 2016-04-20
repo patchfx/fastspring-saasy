5e5bb1a84f90f638c28ba6da8d36be6bf2ccab29
Richard Patching <richard@justaddpixels.com>
2016-04-20 16:44:26 +0100

    Fix whitespace

--- lib/fastspring-saasy/localized_store_pricing.rb
+++ lib/fastspring-saasy/localized_store_pricing.rb
@@ -7,60 +7,60 @@ module FastSpring
       @response = self.class.get(base_localized_store_pricing_path, :query => query, :parser => Parser::Txt)
       self
     end
-    
+
     def base_localized_store_pricing_path
       "/#{@company}/api/price"
     end
-    
+
     def query
       query_hash = Hash.new
       @product_paths.each_index{ |index| query_hash["product_#{(index + 1)}_path".to_sym] = @product_paths[index] }
-      query_hash.merge({ 
+      query_hash.merge({
         :user_remote_addr => @remote_ip,
         :user_accept_language => @http_accept_language,
         :user_x_forwarded_for => @http_x_forwarded_for
       })
     end
-    
+
     def user_country
       parsed_response['user_country']
     end
-    
+
     def user_language
       parsed_response['user_language']
     end
-    
+
     def user_currency
       parsed_response['user_currency']
     end
-    
+
     def product_quantity(product_path)
       parsed_response[product_path]["quantity"]
     end
-    
+
     def product_unit_value(product_path)
       parsed_response[product_path]["unit_value"]
     end
-    
+
     def product_unit_currency(product_path)
       parsed_response[product_path]["unit_currency"]
     end
-    
+
     def product_unit_display(product_path)
       parsed_response[product_path]["unit_display"]
     end
-    
+
     def product_unit_html(product_path)
       parsed_response[product_path]["unit_html"]
     end
 
 
     protected
-    
+
     def parsed_response
       @parsed_response ||= parse_response
     end
-    
+
     def parse_response
       response = Hash.new
 
@@ -69,7 +69,7 @@ module FastSpring
       while more_products
         product_path = @response.parsed_response["product_#{index}_path"]
         if product_path
-          @response.parsed_response.each do |key, value| 
+          @response.parsed_response.each do |key, value|
             if key.match("product_#{index}") && key != "product_#{index}_path" then
               response[product_path] ||= Hash.new
               response[product_path][key.match(/\d+_(.*)/)[1]] = value
--- spec/localized_store_pricing_spec.rb
+++ spec/localized_store_pricing_spec.rb
@@ -25,7 +25,7 @@ describe FastSpring::LocalizedStorePricing do
       config[:username] = 'admin'
       config[:password] = 'test'
       config[:company] = 'acme'
-    end    
+    end
   end
 
   context 'url for localized store pricing' do
@@ -35,24 +35,24 @@ describe FastSpring::LocalizedStorePricing do
         stub_request(:get, "http://sites.fastspring.com/acme/api/price?product_1_path=/standard&user_accept_language=nl&user_remote_addr=192.168.1.1&user_x_forwarded_for=192.168.1.2").
           to_return(:status => 200, :body => "", :headers => {})
       end
-    
+
       it 'returns the path for the company' do
         subject.base_localized_store_pricing_path.should == "/acme/api/price"
       end
     end
-    
+
     context "with invalid http request" do
       subject { FastSpring::LocalizedStorePricing.find(['/standard'], invalid_http_request) }
       before do
         stub_request(:get, "http://sites.fastspring.com/acme/api/price?product_1_path=/standard&user_accept_language=en&user_remote_addr=127.0.0.1&user_x_forwarded_for=").
           to_return(:status => 200, :body => "", :headers => {})
       end
-    
+
       it 'returns the path for the company' do
         subject.base_localized_store_pricing_path.should == "/acme/api/price"
       end
     end
-    
+
     context "with partial http request" do
       subject { FastSpring::LocalizedStorePricing.find(['/standard'], partial_http_request) }
       before do
@@ -65,86 +65,86 @@ describe FastSpring::LocalizedStorePricing do
       end
     end
   end
-  
+
   context "parsed response for 1 product" do
     subject { FastSpring::LocalizedStorePricing.find(['/standard'], valid_http_request) }
     before do
       stub_request(:get, "https://api.fastspring.com/bizplay/api/price?product_1_path=/standard&user_accept_language=nl&user_remote_addr=192.168.1.1&user_x_forwarded_for=192.168.1.2").
         to_return(stub_http_response_with('basic_localized_store_pricing.txt'))
     end
-  
+
     it 'returns "US" as the user country' do
       subject.user_country.should == "US"
     end
-  
+
     it 'returns "en" as the user language' do
       subject.user_language.should == "en"
     end
-    
+
     it 'returns "USD" as the user currency' do
       subject.user_currency.should == "USD"
     end
-    
+
     it 'returns "1" as the quantity of product 1' do
       subject.product_quantity("/standard").should == "1"
     end
-    
+
     it 'returns "35.00" as unit value of product 1' do
       subject.product_unit_value("/standard").should == "35.00"
     end
-    
+
     it 'returns "USD" as the unit currency of product 1' do
       subject.product_unit_currency("/standard").should == "USD"
     end
-    
+
     it 'returns "$35.00" as the unit display of product 1' do
       subject.product_unit_display("/standard").should == "$35.00"
     end
-    
+
     it 'returns "$35.00" as the unit html of product 1' do
       subject.product_unit_html("/standard").should == "$35.00"
     end
   end
-  
+
   context "localized pricing details specifically for 3 product" do
     subject { FastSpring::LocalizedStorePricing.find(['/basic','/standard','/plus'], valid_http_request) }
     before do
       stub_request(:get, "http://sites.fastspring.com/acme/api/price?product_1_path=/basic&product_2_path=/standard&product_3_path=/plus&user_accept_language=nl&user_remote_addr=192.168.1.1&user_x_forwarded_for=192.168.1.2").
         to_return(stub_http_response_with('basic_localized_store_pricing_with_3_products.txt'))
     end
-    
+
     it 'it sends "/basic" as the product_1_path' do
       subject.query[:product_1_path].should == "/basic"
     end
-  
+
     it 'it sends "/standard" as the product_2_path' do
       subject.query[:product_2_path].should == "/standard"
     end
-  
+
     it 'it sends "/plus" as the product_3_path' do
       subject.query[:product_3_path].should == "/plus"
     end
-  
+
     it 'returns "US" as the user country' do
       subject.user_country.should == "US"
     end
-  
+
     it 'returns "en" as the user language' do
       subject.user_language.should == "en"
     end
-    
+
     it 'returns "USD" as the user currency' do
       subject.user_currency.should == "USD"
     end
-    
+
     it 'returns "$19.00" as the unit display of product 1' do
       subject.product_unit_display("/basic").should == "$19.00"
     end
-    
+
     it 'returns "$35.00" as the unit display of product 2' do
       subject.product_unit_display("/standard").should == "$35.00"
     end
-    
+
     it 'returns "$59.00" as the unit display of product 3' do
       subject.product_unit_display("/plus").should == "$59.00"
     end
